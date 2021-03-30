9bdc99b2b8fc056b115c1014f320e428556f7d6a
ankit <developer4u.anky@gmail.com>
2021-03-30 18:41:48 +0530

    added page card

--- src/App.js
+++ src/App.js
@@ -5,19 +5,13 @@ import './assets/css/style.css';
 import Login from './components/Login';
 // import Register from './components/Register';
 import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
-import { mobileValidation, mPinValidation} from './validation';
-
-const validate = {
-  mobile: mobileValidation,
-  mpin: mPinValidation,
-};
 
 function App() {
   return (
     <div className="wrapper">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" validate={validate} component={Login} />
+          <Route exact path="/" component={Login} />
         </Switch>
       </BrowserRouter>
     </div>
